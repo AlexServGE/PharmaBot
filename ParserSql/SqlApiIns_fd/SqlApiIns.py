d0d5176ba9249ff307f8a4f5db65085c8f2c2b12
AlexServGE <alexander.sergeev@yandex.ru>
2023-07-20 12:52:29 +0300

    Dockerfile corrected with WORKDIR

--- ParserSql/SqlApiIns_fd/SqlApiIns.py
+++ ParserSql/SqlApiIns_fd/SqlApiIns.py
@@ -11,7 +11,7 @@ class SqlApiIns:
 
     def establish_sql_connection(self):
         try:
-            con = sqlite3.connect("./ProcurementsDB/Procurements.db")
+            con = sqlite3.connect("../ProcurementsDB/Procurements.db")
             print("Connection is established: Database is created in memory")
             return con
         except Error:
